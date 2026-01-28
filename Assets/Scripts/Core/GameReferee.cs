fc77881e497723e7401d759d65175c22e6aeb271
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-01-29 03:03:29 +0900

    refactor: ラインを越えているかの判定をStoneからGameRefereeに移動

---
+++ Assets/Scripts/Core/GameReferee.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+// ゲームの審判
+public class GameReferee
+{
+    private readonly Stone stone;
+    private readonly GameObject line;
+
+    public GameReferee(Stone stone, GameObject line)
+    {
+        this.stone = stone;
+        this.line = line;
+    }
+
+    // 加速ができるか判定
+    public bool CanAccelerate()
+    {
+        return !IsOverLine();
+    }
+
+    // ゲームは終了したか判定
+    public bool IsGameOver()
+    {
+        return IsOverLine() && stone.IsStop();
+    }
+
+    // ボーダーラインを超えたか判定
+    bool IsOverLine()
+    {
+        if (line == null) return false; // lineが未設定の場合falseを返す
+        return stone.CurrentX > line.transform.position.x;
+    }
+}
