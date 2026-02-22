3a078ca0930bc56e5153ba6422999e69fcd3a2ff
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-02-22 16:51:15 +0900

    feat: 設定をキーボードで変更しパネルを閉じれるよう改変

--- Assets/Scripts/Title/OptionPanelToggle.cs
+++ Assets/Scripts/Title/OptionPanelToggle.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 // OptionPanelの表示状態を切り替える
 public class OptionPanelToggle : MonoBehaviour
 {
     [SerializeField] private GameObject optionPanel;
+    [SerializeField] private GameObject initialSelectedObject;
+    [SerializeField] private GameObject closedSelectedObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,11 +18,25 @@ public class OptionPanelToggle : MonoBehaviour
     public void OpenOption()
     {
         optionPanel.SetActive(true);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (initialSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(initialSelectedObject);
+        }
     }
 
     // 設計パネルを閉じる
     public void CloseOption()
     {
         optionPanel.SetActive(false);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (closedSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(closedSelectedObject);
+        }
     }
 }
