28f417ce15a1e9ddb20b3e1c56947f71cb5d07c3
山口創也 <souyayamaguchi.0803@gmail.com>
2026-02-22 17:32:51 +0900

    feat; キーボード操作の充実化 (#22)
    - Event System の First Selected を設定し、タイトルへのシーン遷移時にスタートボタンに自動でフォーカスされる機能を追加
    - オプションパネルの開閉時に適切なオブジェクトに自動でフォーカスされる機能を追加
      - 開くときはBGMスライダー
      - 閉じるときは設定ボタン
    - リザルト表示時にリトライボタンに自動でフォーカスされる機能を追加
    - BGM Slider の Navigation を Explicit にし、遷移先を意図したものに固定
    - ESC キーでオプションを閉じるショートカットを追加
    
    キーボード操作のみでのプレイを実現。
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

--- Assets/Scripts/Title/OptionPanelToggle.cs
+++ Assets/Scripts/Title/OptionPanelToggle.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 // OptionPanelの表示状態を切り替える
 public class OptionPanelToggle : MonoBehaviour
 {
     [SerializeField] private GameObject optionPanel;
+    [SerializeField] private GameObject initialSelectedObject;
+    [SerializeField] private GameObject closedSelectedObject;
+
+    private bool PressedEscape => Keyboard.current != null
+                                && Keyboard.current.escapeKey.wasPressedThisFrame;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,15 +18,38 @@ public class OptionPanelToggle : MonoBehaviour
         optionPanel.SetActive(false);
     }
 
+    void Update()
+    {
+        // パネルが開いている状態の時に、ESCキーが押されたら閉じる
+        if (optionPanel.activeSelf && PressedEscape)
+        {
+            CloseOption();
+        }
+    }
+
     // 設計パネルを開く
     public void OpenOption()
     {
         optionPanel.SetActive(true);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (initialSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(initialSelectedObject);
+        }
     }
 
     // 設計パネルを閉じる
     public void CloseOption()
     {
         optionPanel.SetActive(false);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (closedSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(closedSelectedObject);
+        }
     }
 }
