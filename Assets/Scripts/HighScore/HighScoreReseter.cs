3296dcab8cb6100ff5ae4a6137bd77d3eb989cd7
山口創也 <souyayamaguchi.0803@gmail.com>
2026-02-22 18:35:29 +0900

    feat: リセット時に確認のポップアップを表示するよう変更 (#23)
    - 確認用のポップアップパネルを作成
    - リセットボタンを押したとき、ポップアップを表示するよう変更
    - 「はい」を押すとリセットを行いポップアップを閉じる機能を追加
    - 「いいえ」を押すとリセットせずポップアップを閉じる機能を追加
    - ポップアップの開閉時に適切なオブジェクトに自動でフォーカスする機能を追加
      - 開いたときは「いいえ」ボタン
      - 閉じた時はリセットボタン
    
    誤操作によるリセットの発生を抑制するためポップアップを追加。
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

--- Assets/Scripts/HighScore/HighScoreReseter.cs
+++ Assets/Scripts/HighScore/HighScoreReseter.cs
@@ -1,9 +1,44 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HighScoreReseter : MonoBehaviour
 {
     // 必要な場合、ハイスコアを描画するクラスへの参照を保持
     [SerializeField] private HighScoreDisplay highScoreDisplay;
+    [SerializeField] private GameObject confirmPanel;
+    [SerializeField] private GameObject initialSelectedObject;
+    [SerializeField] private GameObject closedSelectedObject;
+
+    void Start()
+    {
+        confirmPanel.SetActive(false);
+    }
+
+    // 確認パネルを開く
+    public void OpenConfirmPanel()
+    {
+        confirmPanel.SetActive(true);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (initialSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(initialSelectedObject);
+        }
+    }
+
+    // 確認パネルを閉じる
+    public void CloseConfirmPanel()
+    {
+        confirmPanel.SetActive(false);
+
+        // フォーカスを合わせる
+        EventSystem.current.SetSelectedGameObject(null);
+        if (closedSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(closedSelectedObject);
+        }
+    }
 
     // ハイスコアをリセットする
     public void ResetHighScore()
@@ -12,5 +47,8 @@ public class HighScoreReseter : MonoBehaviour
 
         // 必要ならハイスコアを再描画
         if (highScoreDisplay != null) highScoreDisplay.Display();
+
+        // ポップアップを閉じる
+        CloseConfirmPanel();
     }
 }
