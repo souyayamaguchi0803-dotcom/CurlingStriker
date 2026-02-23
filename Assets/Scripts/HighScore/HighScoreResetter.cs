98b0fcd2d1fa3174b88f9c5169be2a7fc24aa043
山口創也 <souyayamaguchi.0803@gmail.com>
2026-02-23 14:27:09 +0900

    refactor: パネルの開閉ロジックの抽出 (#36)
    - HighScoreResetter から確認パネルの開閉の責任の分離
    - OptionPanelToggle を PanelToggle にリネームし汎用性向上
      - フィールドやメソッド等も同時にリネーム
    - 確認パネルの開閉を PanelToggle を利用するように変更
      - これに伴い、確認パネルも ESC で閉じるよう仕様変更
    - ヒエラルキーの構造を目的別になるよう変更
      - このとき、それぞれの機能を空オブジェクト（Option と HighScore Reset）でまとめた
      - 階層構造の変化により、オブジェクトの位置が僅かにずれている可能性あり
    - スクリプトをアタッチするオブジェクトをCanvas内の空オブジェクトに変更
    - Option と HighScore Reset をそれぞれプレハブ化
    
    責任の分離と合流を行った。
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

--- Assets/Scripts/HighScore/HighScoreResetter.cs
+++ Assets/Scripts/HighScore/HighScoreResetter.cs
@@ -5,28 +5,6 @@ public class HighScoreResetter : MonoBehaviour
 {
     // 必要な場合、ハイスコアを描画するクラスへの参照を保持
     [SerializeField] private HighScoreDisplay highScoreDisplay;
-    [SerializeField] private GameObject confirmPanel;
-    [SerializeField] private GameObject initialSelectedObject;
-    [SerializeField] private GameObject closedSelectedObject;
-
-    void Start()
-    {
-        confirmPanel.SetActive(false);
-    }
-
-    // 確認パネルを開く
-    public void OpenConfirmPanel()
-    {
-        confirmPanel.SetActive(true);
-        FocusSetter.Set(initialSelectedObject);
-    }
-
-    // 確認パネルを閉じる
-    public void CloseConfirmPanel()
-    {
-        confirmPanel.SetActive(false);
-        FocusSetter.Set(closedSelectedObject);
-    }
 
     // ハイスコアをリセットする
     public void ResetHighScore()
@@ -35,8 +13,5 @@ public class HighScoreResetter : MonoBehaviour
 
         // 必要ならハイスコアを再描画
         if (highScoreDisplay != null) highScoreDisplay.Display();
-
-        // ポップアップを閉じる
-        CloseConfirmPanel();
     }
 }
