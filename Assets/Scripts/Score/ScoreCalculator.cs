253848a43571e620b39e39c59716c45ecef98ac4
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-11 18:16:16 +0900

    refactor: 位置を表すプロパティを追加

--- Assets/Scripts/Score/ScoreCalculator.cs
+++ Assets/Scripts/Score/ScoreCalculator.cs
@@ -5,9 +5,12 @@ public class ScoreCalculator : MonoBehaviour
     [SerializeField] private GameObject stone;
     [SerializeField] private GameObject house;
 
+    private Vector2 stonePosition => stone.transform.position;
+    private Vector2 housePosition => house.transform.position;
+
     public Score Calculate()
     {
-        float distance = Vector2.Distance(stone.transform.position, house.transform.position);
+        float distance = Vector2.Distance(stonePosition, housePosition);
         return new Score(distance);
     }
 }
