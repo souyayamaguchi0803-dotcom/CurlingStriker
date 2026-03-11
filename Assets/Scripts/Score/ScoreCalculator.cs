c578ed2c2cae0e0139a6876ee0c29fd78c8971c3
山口創也 <souyayamaguchi.0803@gmail.com>
2026-03-11 18:29:20 +0900

    refactor: スコアの計算ロジックの移動 (#75)
    - 計算を行う Score コンストラクタを削除
    - スコアの計算を ScoreCalculator に移動
    - コメントを追加
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

--- Assets/Scripts/Score/ScoreCalculator.cs
+++ Assets/Scripts/Score/ScoreCalculator.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 
+// スコアの計算を行うクラス
 public class ScoreCalculator : MonoBehaviour
 {
     [SerializeField] private GameObject stone;
     [SerializeField] private GameObject house;
 
+    // 座標を表すプロパティ
+    private Vector2 stonePosition => stone.transform.position;
+    private Vector2 housePosition => house.transform.position;
+
+    // ストーンとハウスの距離を計算しスコアへ変換
     public Score Calculate()
     {
-        return new Score(stone.transform.position, house.transform.position);
+        float distance = Vector2.Distance(stonePosition, housePosition);
+        return new Score(distance);
     }
 }
